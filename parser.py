22bef12b5999006c0b9121a71fde350f7a1c62bd
Mordecaikama <agyapongmordecai@gmail.com>
2025-07-23 23:44:31 +0100

    files updated

--- parser.py
+++ parser.py
@@ -1,8 +1,6 @@
 # Log Parsing Logic
 import re
 
-from database import createTable, createConnection,insertData
-from summarizer import saveTjson,saveTocsv
 
 
 log_pattern = re.compile(
@@ -41,20 +39,3 @@ def extractLogs(data:list) -> list:
 
 
 
-def main():
-    try:
-        logsfile = './data/logs/apache_logs'
-        data = readfile(logsfile)
-        parsed_logs = extractLogs(data)
-        cur,conn = createConnection()
-        table = createTable(cur)
-        insertData(conn,cur,parsed_logs)
-        saveTocsv(parsedLogs=parsed_logs)
-        saveTjson(parsedLogs=parsed_logs)
-    except BaseException as error:
-        print(error)
-
-
-
-if __name__ == '__main__':
-    main()
