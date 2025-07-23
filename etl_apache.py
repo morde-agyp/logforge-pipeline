d3b5ecc07462ed03a018d5a06826d83cac8044ed
Mordecaikama <agyapongmordecai@gmail.com>
2025-07-23 23:44:05 +0100

    etl file added

--- etl_apache.py
+++ etl_apache.py
@@ -1,1 +1,26 @@
 # Entry-point CLI
+
+from database import createTable, createConnection,insertData
+from summarizer import saveTjson,saveTocsv
+from parser import readfile, extractLogs
+
+
+
+
+def main():
+    try:
+        logsfile = './data/logs/apache_logs'
+        data = readfile(logsfile)
+        parsed_logs = extractLogs(data)
+        cur,conn = createConnection()
+        table = createTable(cur)
+        insertData(conn,cur,parsed_logs)
+        saveTocsv(parsedLogs=parsed_logs)
+        saveTjson(parsedLogs=parsed_logs)
+    except BaseException as error:
+        print(error)
+
+
+
+if __name__ == '__main__':
+    main()
